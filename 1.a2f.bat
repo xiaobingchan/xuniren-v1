b9e8a10930f601f296fbc3071bbb4e9cad32e03a
21452120133157267 <1290851757@qq.com>
2024-07-14 10:40:22 +0800

    update

--- 1.a2f.bat
+++ 1.a2f.bat
@@ -1,4 +1,4 @@
 @echo off
 ping -n 2 127.0.0.1
-start C:\Users\Administrator\Desktop\AudioToLipsyncServer\AudioToLipsyncServer\Engine\Binaries\Win64\AudioToLipsyncServer.exe
+start E:\AudioToLipsyncServer\AudioToLipsyncServer\Engine\Binaries\Win64\AudioToLipsyncServer.exe
 exit
